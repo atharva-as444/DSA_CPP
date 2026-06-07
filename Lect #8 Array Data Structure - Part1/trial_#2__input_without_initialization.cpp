49d674145cce7d998c97bc54378514a21da64d93
atharva-as444 <atharvaas444@gmail.com>
2026-06-08 00:20:52 +0530

    Add initial implementation of input and output for array operations

---
+++ Lect #8 Array Data Structure - Part1/trial_#2__input_without_initialization.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+int marks [6];
+int size =6;
+
+for(int i=0; i<size;i++){
+
+cin>>marks[i];
+}
+
+
+for(int i=0; i<size;i++){
+
+cout<<marks[i]<<endl;
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
