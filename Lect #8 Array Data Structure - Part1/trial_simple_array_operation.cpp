0ceb9e3d9fca733c9738031976a96ada60ee34ef
atharva-as444 <atharvaas444@gmail.com>
2026-06-07 14:29:56 +0530

    Fix output formatting in trial_simple_array_operation for consistency

--- Lect #8 Array Data Structure - Part1/trial_simple_array_operation.cpp
+++ Lect #8 Array Data Structure - Part1/trial_simple_array_operation.cpp
@@ -13,11 +13,11 @@ int main() {
     // modify and print array values
     marks[0] = 27; //called array marks function here
 
-    cout <<"Function marks array index input index value is: "<<marks[0]<< "\n";
+    cout <<"Function marks array index input index value is: "<<marks[0]<<endl;
 
 
     int simple[] = {1, 2, 3, 4, 5}; 
-    cout<<" simple Array index input index value is: "<<simple[2];  /* print any value from the array using the index of array.
+    cout<<" simple Array index input index value is: "<<simple[2]<<endl;  /* print any value from the array using the index of array.
      For example if array index = 0 ---> it will print 1st value given inside array...  the value can be any number or othrr data type
      but the array index always starts from 0. 
      That's why when we read loops we have be told to adopt the writing of loops value starting from no 0 */
