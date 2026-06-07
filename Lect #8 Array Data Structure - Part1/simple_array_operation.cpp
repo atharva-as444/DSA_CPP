c644516c2588e48d36a9f325ac9e0c4eca44123c
atharva-as444 <atharvaas444@gmail.com>
2026-06-07 14:06:06 +0530

    Add simple array operation example with output

---
+++ Lect #8 Array Data Structure - Part1/simple_array_operation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}; 
+     cout<<arr[2];  /* print any value from the array using the index of array.
+     For example if array index = 0 ---> it will print 1st value given inside array...  the value can be any number or othrr data type
+     but the array index always starts from 0. 
+     That's why when we read loops we have be told to adopt the writing of loops value starting from no 0 */
+     return 0;
+}
+
+
+
