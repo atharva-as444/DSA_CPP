1ce81c1a81684945a5d8b281c0424d1a1b0244e0
atharva-as444 <atharvaas444@gmail.com>
2026-06-07 14:26:52 +0530

    Refactor output messages in trial_simple_array_operation for clarity

--- Lect #8 Array Data Structure - Part1/trial_simple_array_operation.cpp
+++ Lect #8 Array Data Structure - Part1/trial_simple_array_operation.cpp
@@ -11,12 +11,13 @@ int marks[5] = {2,3,4,5,6};  //function of array
 
 int main() {
     // modify and print array values
-    marks[0] = 27;
-    cout << marks[0]<< "\n";
+    marks[0] = 27; //called array marks function here
+
+    cout <<"Function marks array index input index value is: "<<marks[0]<< "\n";
 
 
-    int arr[] = {1, 2, 3, 4, 5}; 
-    cout<<arr[2];  /* print any value from the array using the index of array.
+    int simple[] = {1, 2, 3, 4, 5}; 
+    cout<<" simple Array index input index value is: "<<simple[2];  /* print any value from the array using the index of array.
      For example if array index = 0 ---> it will print 1st value given inside array...  the value can be any number or othrr data type
      but the array index always starts from 0. 
      That's why when we read loops we have be told to adopt the writing of loops value starting from no 0 */
